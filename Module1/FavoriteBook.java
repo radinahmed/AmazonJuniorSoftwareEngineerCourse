003c66c60594ec4e70674b2beb4499df0912fa36
Radin Ahmed <44245581+radinahmed@users.noreply.github.com>
2024-11-23 20:31:32 -0500

    Create FavoriteBook.java

---
+++ Module1/FavoriteBook.java
@@ -0,0 +1,17 @@
+public class FavoriteBook {
+    public static void main(String args[]) {
+        String Title = "Charlie and the Chocolate Factory";
+        String Author = "Roald Dahl";
+        int Pages = 300;
+        Double Price = 20.22;
+
+
+	//TODO: Your code goes here
+        System.out.println("Title of the book: " + Title);
+       System.out.println("Author of the Book is: " + Author );
+       System.out.println("Pages in the book: " + Pages);
+       System.out.println("Price of the book: " + Price);
+       
+
+    }
+}
