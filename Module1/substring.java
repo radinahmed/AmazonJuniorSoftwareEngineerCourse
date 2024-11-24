2a7d89d1ed6a549c182a85dd0d78e7ea625728dd
Radin Ahmed <44245581+radinahmed@users.noreply.github.com>
2024-11-24 15:03:58 -0500

    Create substring.java

---
+++ Module1/substring.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String args[]) {
+        String text = "subscription";
+        String extractedText = text.substring(3, 9);
+        
+        System.out.println("Output: " + extractedText);
+    }
+}
