201ce4c6292d2396852000c14f52280e037842d1
Radin Ahmed <44245581+radinahmed@users.noreply.github.com>
2024-11-23 19:00:12 -0500

    Update MyFirstProgram.java

--- Module1/MyFirstProgram.java
+++ Module1/MyFirstProgram.java
@@ -1,5 +1,6 @@
 public class MyFirstProgram {
     public static void main(String[] args){
-        System.out.println("I'm a Programmer");
+        System.out.println("Let's change the code now...");
+        System.out.println("it's what I do!");
     }
 }
